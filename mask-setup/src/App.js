91992e1e5ca79795f0a8af5b1895c725affd7967
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-05-02 10:44:01 -0500

    Update

--- mask-setup/src/App.js
+++ mask-setup/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from './views/Navbar'
 import Wallet from './views/Wallet';
+import {ethers} from 'ethers';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  const checkBrowser = () => {
+    if(window.ethereum ){
+      console.log("Browser supports web3");
+    } else {
+      console.log("Browser does not support web3");
+    }
+  }
+
   return (
     <div>
       <header>
@@ -10,7 +22,7 @@ function App() {
       </header>
 
       <div className='container mx-auto'>      
-        <Wallet/>
+        <Wallet web3={checkBrowser}/>
       </div>
 
     </div>
